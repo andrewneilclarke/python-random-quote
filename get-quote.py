0873bd147e347fc6b6f2102a65c64a84bfeef458
Andrew <andy@Annas-MacBook-Pro-2.local>
2020-08-25 21:32:00 +0200

    Hello World

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def Haupt():
-  print("Keep it logically awesome.")
+  #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   Haupt()
